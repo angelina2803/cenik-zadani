7661304e303f1b3c05007805dd543f0a852a0c3e
Angelina Yumatova <>
2023-03-25 11:07:40 +0100

    change

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-const selectPlan = (planNumber) =>{
-     let classChange = document.querySelector('.plan')
-     classChange.classList.add('plan--selected') = planNumber
+
+const selectPlan = (planNumber) => {
+     if (planNumber === 1){
+
+          document.querySelector('#plan1').classList.add('plan--selected')
+          
+     } else if (planNumber === 2){
+
+          document.querySelector('#plan2').classList.add('plan--selected')
+          document.querySelector('#plan1').classList.toggle('plan--selected')
+          
+     } else if(planNumber === 3){
+
+          document.querySelector('#plan3').classList.add('plan--selected')
+          document.querySelector('#plan1').classList.toggle('plan--selected')
+          
+     } 
 }
-selectPlan()
+selectPlan(2)

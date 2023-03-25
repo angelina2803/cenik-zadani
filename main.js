e258fdff7939e723671183158c430e12147d652b
Angelina Yumatova <>
2023-03-25 11:45:01 +0100

    change

--- main.js
+++ main.js
@@ -3,16 +3,20 @@ const selectPlan = (planNumber) => {
      if (planNumber === 1){
 
           document.querySelector('#plan1').classList.add('plan--selected')
+          document.querySelector('#plan2').classList.remove('plan--selected')
+          document.querySelector('#plan3').classList.remove('plan--selected')
           
      } else if (planNumber === 2){
 
           document.querySelector('#plan2').classList.add('plan--selected')
-          document.querySelector('#plan1').classList.toggle('plan--selected')
+          document.querySelector('#plan1').classList.remove('plan--selected')
+          document.querySelector('#plan3').classList.remove('plan--selected')
           
      } else if(planNumber === 3){
 
           document.querySelector('#plan3').classList.add('plan--selected')
-          document.querySelector('#plan1').classList.toggle('plan--selected')
+          document.querySelector('#plan1').classList.remove('plan--selected')
+          document.querySelector('#plan2').classList.add('plan--selected')
           
      } 
 }
